d7ab7e0feef29ac01a86d3370b2ee83e343eb81c
wangxingyu <330886959@qq.com>
2023-02-15 09:15:15 +0800

    首次提交

---
+++ src/main/java/cn/eric/appautotest/AppAutoTestApplication.java
@@ -0,0 +1,13 @@
+package cn.eric.appautotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAutoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppAutoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/eric/appautotest/AppAutoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.eric.appautotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAutoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
